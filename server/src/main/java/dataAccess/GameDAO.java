9fae20112c04bb71bff2b379923aad2af2495712
Sean Stidham <seanstidham14@gmail.com>
2024-02-19 13:12:25 -0700

    Think I implemented GameDAO correct but we'll see

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -1,4 +1,16 @@
 package dataAccess;
 
+import model.GameData;
+
 public interface GameDAO {
+    //okay now with this one we cant delete games but we do need to list them
+    //so itll be more of a CRLU than CRUD i think on this one, at least going off the github specs
+    void createGame(GameData game) throws DataAccessException;
+
+    void getGame(String gameID) throws DataAccessException;
+
+    GameData[] listGames() throws DataAccessException;
+
+    void updatedGame(String gameID, GameData updatedGameData) throws DataAccessException;
+
 }
