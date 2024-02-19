1cb0dc5ac894ec8c8bdbb3e1004d2df5844efda3
Sean Stidham <seanstidham14@gmail.com>
2024-02-19 13:06:18 -0700

    fixed to an interfaceas in the specs and implemented AuthDAO

---
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -0,0 +1,15 @@
+package dataAccess;
+
+import model.AuthData;
+
+public interface AuthDAO {
+    //So looking at the github specs i need to throw exceptions and build a crud framework
+
+    //create
+    void createAuth(AuthData auth) throws DataAccessException;
+    //read
+    AuthData getAuth(String authToken) throws DataAccessException;
+    //no update with this one so on to delete
+    void deleteAuth(String authToken) throws DataAccessException;
+
+}
