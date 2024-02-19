dece326b550eca16fc79c895ae7581ff84234601
Sean Stidham <seanstidham14@gmail.com>
2024-02-19 13:17:29 -0700

    UserDOA implemented

--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -1,4 +1,14 @@
 package dataAccess;
 
+import model.UserData;
+
 public interface UserDAO {
+    //so the specs only say I need a create and get for this one, but pretty sure imma need the whole CRUD method so 2 people cant have the same name and I can delete it
+    void createUser(UserData user) throws DataAccessException;
+
+    UserData getUser(String username) throws DataAccessException;
+
+    void updateUser(UserData user) throws DataAccessException;
+
+    void deleteUser(String username) throws DataAccessException;
 }
