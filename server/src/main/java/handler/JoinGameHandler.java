68be891033dc91090f5ac609246eccc973cb7dcd
Sean Stidham <seanstidham14@gmail.com>
2024-02-25 15:23:26 -0700

    got StandardAPI Tests working, now I just need to test all my own test files

--- server/src/main/java/handler/JoinGameHandler.java
+++ server/src/main/java/handler/JoinGameHandler.java
@@ -22,7 +22,7 @@ public class JoinGameHandler {
         JoinGameRequest joinGameRequest = gson.fromJson(request.body(), JoinGameRequest.class);
         String authToken = request.headers("authorization");
 
-        if (joinGameRequest == null || joinGameRequest.gameID() == null) {
+        if (joinGameRequest == null || joinGameRequest.gameID() == null || joinGameRequest.gameID() < 1) {
             response.status(400);
             return gson.toJson(new JoinGameResult("Error: bad request"));
         }
--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -12,39 +12,37 @@ public class CreateGameService {
     //imma also have to make a request and result file, imma make all the files rn in fact
         private AuthDAO authDAO;
         private GameDAO gameDAO;
-        private int gameCounter;
+        private int gameIDCounter;
 
         public CreateGameService(GameDAO gameDAO, AuthDAO authDAO) {
             this.gameDAO = gameDAO;
             this.authDAO = authDAO;
-            this.gameCounter = 0;
+            this.gameIDCounter = 0;
         }
 
         public CreateGameResult createGame(String authToken, CreateGameRequest request) throws DataAccessException {
-            try {
-                if(authToken == null) {
-                    return new CreateGameResult(null, "Error: unauthorized");
-                }
-
-                if (!isValidAuthToken(authToken)) {
-                    return new CreateGameResult(null, "Error: unauthorized");
-                }
-
-                if (request == null || request.gameName() == null) {
-                    return new CreateGameResult(null, "Error: bad request");
-                }
-
-                ChessGame chessGame = new ChessGame();
-
-                GameData newGame = new GameData(gameCounter, null,null, request.gameName(), chessGame);
-                GameData createdGame = gameDAO.createGame(newGame);
-
-                gameCounter++;
-
-                return new CreateGameResult(createdGame.gameID(), null);
-            } catch (DataAccessException e) {
-                throw e;
+            if(authToken == null) {
+                return new CreateGameResult(null, "Error: unauthorized");
             }
+
+            if (!isValidAuthToken(authToken)) {
+                return new CreateGameResult(null, "Error: unauthorized");
+            }
+
+            if (request == null || request.gameName() == null) {
+                return new CreateGameResult(null, "Error: bad request");
+            }
+            gameIDCounter++;
+            //found the error, the games were being defaulted to 0 instead of 1 when it wasnt specified, thought I covered it but
+            //i messed up the increment of it all
+            ChessGame chessGame = new ChessGame();
+
+            GameData newGame = new GameData(gameIDCounter, null,null, request.gameName(), chessGame);
+            GameData createdGame = gameDAO.createGame(newGame);
+
+
+
+            return new CreateGameResult(createdGame.gameID(), null);
         }
 
         private boolean isValidAuthToken(String authToken) throws DataAccessException {
