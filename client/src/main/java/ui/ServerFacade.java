3fe4ca219ab27d7c55953732d34d4d728623f711
Sean Stidham <seanstidham14@gmail.com>
2024-03-14 17:06:07 -0600

    if i lose it all

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,4 +1,59 @@
 package ui;
-
+import com.google.gson.*;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.HashMap;
+import java.util.Map;
 public class ServerFacade {
+    //okay first the imports
+    //for list, join, and join observ imma need a way to store the stuff and the only way ik how to do that is with Hashmaps
+    private Map<Integer, Integer> crumblingfarumAzula; //sorry been playing a lot of Elden Ring lately
+    private static String BASE_URL = "http://localhost:8080";
+    public ServerFacade() {
+        this.crumblingfarumAzula = new HashMap<>();
+    }
+    //okay i wanna try something lemme cook on this
+    //my idea is to separate the HTTP connection is 2 different methods and have each one call them to handle the setup and the execution of it
+    //let me cook this might be good or it might be bad
+    private HttpURLConnection prepareConnection(String endpoint, String method, boolean doOutput, String contentType) throws IOException {
+        URL url = new URL(BASE_URL + endpoint);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod(method);
+        conn.setDoOutput(doOutput);
+        conn.setRequestProperty("Content-type", contentType);
+        return conn;
+    }
+    private String executeRequest(HttpURLConnection conn, JsonObject requestData) throws IOException {
+        if (conn == null)
+            return null;
+
+        if (requestData != null) {
+            String jsonData = new Gson().toJson(requestData);
+            conn.getOutputStream().write(jsonData.getBytes());
+        }
+        conn.connect();
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode == 200) {
+            return conn.getHeaderField("Authorization");
+        } else {
+            InputStreamReader inputStreamReader = new InputStreamReader(conn.getErrorStream());
+            JsonObject errorResponse = JsonParser.parseReader(inputStreamReader).getAsJsonObject();
+            String errorMessage = errorResponse.get("message").getAsString();
+            System.out.println(errorMessage);
+        }
+        return null;
+    }
+    //okay so now i need to see if I can set up all of these functions
+    //login
+    //register
+    //logout
+    //create game
+    //list games
+    //join game
+    //join as observer
+
 }
