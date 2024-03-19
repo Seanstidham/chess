2b2cc15cc4c358683982872649fc9fd480c0b776
Seanstidham <123608245+Seanstidham@users.noreply.github.com>
2024-03-19 17:15:18 -0600

    Update Main.java

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
+import ui.ChessClient;
+import java.rmi.ServerError;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        new ChessClient();
     }
 }
