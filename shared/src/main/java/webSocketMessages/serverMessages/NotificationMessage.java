8342d1321c025a06cca4bcf359aa09555791555b
Sean Stidham <seanstidham14@gmail.com>
2024-04-01 16:37:59 -0600

    NotificationMessage implemented to the github reqs

--- shared/src/main/java/webSocketMessages/serverMessages/NotificationMessage.java
+++ shared/src/main/java/webSocketMessages/serverMessages/NotificationMessage.java
@@ -1,4 +1,22 @@
 package webSocketMessages.serverMessages;
 
-public class NotificationMessage  {
+public class NotificationMessage extends ServerMessage  {
+
+//    Command
+//    NOTIFICATION
+//    Required Fields
+//    String message
+//    Description
+//    This is a message meant to inform a player when another player made an action.
+
+    String notificationMessage;
+
+    public NotificationMessage(String notificationMessage) {
+        super(ServerMessageType.NOTIFICATION);
+        this.notificationMessage = notificationMessage;
+    }
+
+    public String getNotificationMessage() {
+        return notificationMessage;
+    }
 }
