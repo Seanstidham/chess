89ce2333743b2e59ee1e6c8752097c545695a1e4
Sean Stidham <seanstidham14@gmail.com>
2024-04-01 17:34:57 -0600

    LeaveCommand implemented to github reqs

--- shared/src/main/java/webSocketMessages/userCommands/LeaveCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/LeaveCommand.java
@@ -1,4 +1,25 @@
 package webSocketMessages.userCommands;
 
-public class LeaveCommand {
+public class LeaveCommand extends UserGameCommand{
+
+//    Command
+//    LEAVE
+//    Required Fields
+//    Integer gameID
+//    Description
+//    Tells the server you are leaving the game so it will stop sending you notifications.
+
+//Each user game command class must include the authToken field and the commandType field and should inherit from the UserGameCommand class.
+// The commandType field must be set to the corresponding CommandType.
+    int gameID;
+
+    public LeaveCommand(int gameID, String authToken) {
+        super(authToken);
+        this.gameID = gameID;
+        this.commandType = CommandType.LEAVE;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
 }
