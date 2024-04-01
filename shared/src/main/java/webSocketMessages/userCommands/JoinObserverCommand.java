d1d64916af5f955a52a268ad229ed2a0f5899906
Sean Stidham <seanstidham14@gmail.com>
2024-04-01 17:08:40 -0600

    Join Observer command implemented to the github reqs

--- shared/src/main/java/webSocketMessages/userCommands/JoinObserverCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/JoinObserverCommand.java
@@ -1,4 +1,26 @@
 package webSocketMessages.userCommands;
 
-public class JoinObserverCommand {
+public class JoinObserverCommand extends UserGameCommand{
+
+//    Command
+//    JOIN_OBSERVER
+//    Required Fields
+//    Integer gameID
+//    Description
+//    Used to request to start observing a game.
+
+//Each user game command class must include the authToken field and the commandType field and should inherit from the UserGameCommand class.
+// The commandType field must be set to the corresponding CommandType.
+
+    int gameID;
+
+    public JoinObserverCommand(int gameID, String authToken) {
+        super(authToken);
+        this.gameID = gameID;
+        this.commandType = CommandType.JOIN_OBSERVER;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
 }
