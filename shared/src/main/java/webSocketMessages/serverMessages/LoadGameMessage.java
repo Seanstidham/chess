a0cc513dd22ec76bde054137536684ba165428e5
Sean Stidham <seanstidham14@gmail.com>
2024-04-01 16:25:07 -0600

    implemented LoadGameMessage to teh github reqs

--- shared/src/main/java/webSocketMessages/serverMessages/LoadGameMessage.java
+++ shared/src/main/java/webSocketMessages/serverMessages/LoadGameMessage.java
@@ -1,4 +1,22 @@
 package webSocketMessages.serverMessages;
 
-public class LoadGameMessage {
+public class LoadGameMessage extends ServerMessage {
+
+//    Command
+//    LOAD_GAME
+//    Required Fields
+//    game (can be any type, just needs to be called game)
+//    Description
+//    Used by the server to send the current game state to a client. When a client receives this message, it will redraw the chess board.
+
+    Object game;
+
+    public LoadGameMessage(Object game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public Object getGame() {
+        return game;
+    }
 }
