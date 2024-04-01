b8329b1babf30dd6257905d629797c53c28e5b1b
Sean Stidham <seanstidham14@gmail.com>
2024-04-01 17:43:35 -0600

    ResignCommand implemented to the Github specs

--- shared/src/main/java/webSocketMessages/userCommands/ResignCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/ResignCommand.java
@@ -1,4 +1,26 @@
 package webSocketMessages.userCommands;
 
-public class ResignCommand {
+public class ResignCommand extends UserGameCommand{
+
+//    Command
+//    RESIGN
+//    Required Fields
+//    Integer gameID
+//    Description
+//    Forfeits the match and ends the game (no more moves can be made).
+
+//Each user game command class must include the authToken field and the commandType field and should inherit from the UserGameCommand class.
+// The commandType field must be set to the corresponding CommandType.
+
+    int gameID;
+
+    public ResignCommand(int gameID, String authToken) {
+        super(authToken);
+        this.gameID = gameID;
+        this.commandType = CommandType.RESIGN;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
 }
