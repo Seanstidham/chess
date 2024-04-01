b982f931c53bddb11b20860b5a723dfe88e57806
Sean Stidham <seanstidham14@gmail.com>
2024-04-01 17:16:10 -0600

    MakeMove implemented to the github specs

--- shared/src/main/java/webSocketMessages/userCommands/MakeMoveCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/MakeMoveCommand.java
@@ -1,4 +1,33 @@
 package webSocketMessages.userCommands;
+import chess.ChessMove;
 
-public class MakeMoveCommand {
+public class MakeMoveCommand extends UserGameCommand{
+
+//    Command
+//    MAKE_MOVE
+//    Required Fields
+//    Integer gameID, ChessMove move
+//    Description
+//    Used to request to make a move in a game.
+
+//Each user game command class must include the authToken field and the commandType field and should inherit from the UserGameCommand class.
+// The commandType field must be set to the corresponding CommandType.
+
+    int gameID;
+    ChessMove actualMove;
+
+    public MakeMoveCommand(int gameID, ChessMove actualMove, String authToken) {
+        super(authToken);
+        this.gameID = gameID;
+        this.actualMove = actualMove;
+        this.commandType = CommandType.MAKE_MOVE;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public ChessMove getActualMove() {
+        return actualMove;
+    }
 }
