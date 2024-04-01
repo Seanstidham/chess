ea6577f5a72f60ac1eef23c4cf5ef45e4cfc89ba
Sean Stidham <seanstidham14@gmail.com>
2024-04-01 17:03:31 -0600

    JoinPlayer implemented to teh github specs

--- shared/src/main/java/webSocketMessages/userCommands/JoinPlayerCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/JoinPlayerCommand.java
@@ -1,4 +1,29 @@
 package webSocketMessages.userCommands;
+import chess.ChessGame;
 
-public class JoinPlayerCommand {
+public class JoinPlayerCommand extends UserGameCommand{
+
+//    Command
+//    JOIN_PLAYER
+//    Required Fields
+//    Integer gameID, ChessGame.TeamColor playerColor
+//    Description
+//    Used for a user to request to join a game.
+    int gameID;
+    ChessGame.TeamColor playerColor;
+
+    public JoinPlayerCommand(int gameID, ChessGame.TeamColor playerColor, String authToken) {
+        super(authToken);
+        this.gameID = gameID;
+        this.playerColor = playerColor;
+        this.commandType = CommandType.JOIN_PLAYER;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public ChessGame.TeamColor getPlayerColor() {
+        return playerColor;
+    }
 }
