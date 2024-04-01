a762fda9aea8cf5e8dd443b7eae160ef75839ac1
Sean Stidham <seanstidham14@gmail.com>
2024-04-01 16:32:29 -0600

    Error message implemented according to the github reqs

--- shared/src/main/java/webSocketMessages/serverMessages/ErrorMessage.java
+++ shared/src/main/java/webSocketMessages/serverMessages/ErrorMessage.java
@@ -1,4 +1,23 @@
 package webSocketMessages.serverMessages;
 
-public class ErrorMessage {
+public class ErrorMessage extends ServerMessage{
+
+
+//    Command
+//    ERROR
+//    Required Fields
+//    String errorMessage
+//    Description
+//    This message is sent to a client when it sends an invalid command. The message must include the word Error.
+
+    String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
